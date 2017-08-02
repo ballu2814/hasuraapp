9ee029bb9bc3f1e8f7fecf69e0710db90d2cee16
ballu2814 <ballu281996@gmail.com>
2017-08-02 13:02:04 +0530

    Made some new changes

--- app/src/server.js
+++ app/src/server.js
@@ -1,11 +1,92 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session=require('express-session');
+var passport=require('passport');
+var localstrategy=require('passport-local').Strategy;
+var expressValidator=require('express-validator');
+var multer=require('multer');
+var upload=multer({dest: '/uploads'});
+var flash = require('connect-flash');
+var bcrypt=require('bcryptjs');
+var mongo=require('mongodb');
+var mongoose=require('mongoose');
+var db=mongoose.connection;
+var index = require('./routes/index');
+var users = require('./routes/users');
+var questions = require('./routes/questions');
+var cookieParser = require('cookie-parser');
 var app = express();
+use=0;
+points=0;
+usernamefound=0;
+emailfound=0;
+auth_token='';
 
-//your routes here
-app.get('/', function (req, res) {
-    res.send("Hello World!");
+app.use(cookieParser());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+//HANDLE FILE UPLOADS
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret:'secret',
+  saveUninitialized:true,
+  resave:true
+}));
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+ 
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+app.use('/', index);
+app.use('/users', users);
+app.use('/questions', questions);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
+app.listen('500');
